f318fe12e6c54bada2c2cc9f25163ada0cb37f1c
Marta Karpisiak <mkarpisiak88@gmail.com>
2021-02-23 16:26:31 +0100

    hello world 2

---
+++ src/main/java/HelloWorld2.java
@@ -0,0 +1,8 @@
+public class HelloWorld2 {                           // za pierwszym razem coś poszło nie tak
+    public static void main(String[] args) {         // sprawdzam dlaczego nie widzę zmian z rep zdalnym
+        System.out.println("Hello World!");
+        System.out.println("Marta");
+        System.out.println(33);
+        System.out.println('K');
+    }
+}
