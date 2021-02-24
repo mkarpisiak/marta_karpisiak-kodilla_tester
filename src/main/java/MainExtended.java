3032363490f5f88c2a7dcd9cbff158ce67f36fec
Marta Karpisiak <mkarpisiak88@gmail.com>
2021-02-24 09:40:42 +0100

    MainExtended

---
+++ src/main/java/MainExtended.java
@@ -0,0 +1,18 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Mam na imię Marta.";
+        int myAge = 33;
+        double height = 1.76;
+        boolean cantFindGoodChar = true;
+        String hobby = "Horseback Riding";
+        int howLong = 12;
+        String moreAboutHorses = "Dlaczego jeżdżę? Bo to jest fantastyczne i mogę moje hobby wykorzystać jako ćwiczenie tutaj;)";
+    System.out.println(myName);
+    System.out.println(myAge);
+    System.out.println(height);
+    System.out.println(cantFindGoodChar);
+    System.out.println(hobby);
+    System.out.println(howLong);
+    System.out.println(moreAboutHorses);
+    }
+}
