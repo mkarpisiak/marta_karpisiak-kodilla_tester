bbc3d9979b9c2c738930e0457c1e37c836979691
Marta Karpisiak <mkarpisiak88@gmail.com>
2021-03-18 16:58:30 +0100

    grades

--- src/main/java/Grades.java
+++ src/main/java/Grades.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Grades {
+public class Grades {
+    private int[] grades;
+    private int size;
+    private double result;
+
+    public Grades() {
+        this.grades = new int[10];
+        this.size=0;
+    }
+
+    public void add(int value) {
+        if (this.size == 10) {
+            return;
+        }
+        this.grades[this.size] = value;
+        this.size++;
+    }
+
+    public int lastNumber (){
+        return (this.grades[this.size]);
+    }
+
+    public double arithmeticMean (){
+        for (int i = 0; i <= this.size; i++) {
+            result= result+ grades[1] % this.size;
+        }
+        return result;
+    }
 }
