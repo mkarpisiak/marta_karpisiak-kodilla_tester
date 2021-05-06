5aa6cd9a3542882889abd56099dcc02298a247b1
Marta Karpisiak <mkarpisiak88@gmail.com>
2021-05-06 12:53:39 +0200

    Job

--- kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Jobs/HeadOfGrowth.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Jobs/HeadOfGrowth.java
@@ -2,20 +2,8 @@ package com.kodilla.abstracts.homework.Jobs;
 
 public class HeadOfGrowth extends Job {
 
-    private int salary;
-    private String responsibilities;
 
-    public int getSalary(){
-        return salary;
-    }
-
-    public String getResponsibilities(){
-        return responsibilities;
-    }
-
-
-    public HeadOfGrowth(int salary, String responsibilities) {
-        this.salary = salary;
-        this.responsibilities = responsibilities;
+    public HeadOfGrowth(String responsibilities, int salary) {
+        super(responsibilities, salary);
     }
 }
--- kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Jobs/OfficeManager.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Jobs/OfficeManager.java
@@ -2,20 +2,8 @@ package com.kodilla.abstracts.homework.Jobs;
 
 public class OfficeManager extends Job {
 
-    private int salary;
-    private String responsibilities;
 
-    public int getSalary(){
-        return salary;
-    }
-
-    public String getResponsibilities(){
-        return responsibilities;
-    }
-
-
-    public OfficeManager(int salary, String responsibilities) {
-        this.salary = salary;
-        this.responsibilities = responsibilities;
+    public OfficeManager(String responsibilities, int salary) {
+        super(responsibilities, salary);
     }
 }
