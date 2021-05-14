42714708f2f34cadfc1a825520b195c81337f007
Marta Karpisiak <mkarpisiak88@gmail.com>
2021-05-14 20:20:26 +0200

    1

--- kodilla-collections/src/main/java/com/kodilla/collections/interfaces/Triangle.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/interfaces/Triangle.java
@@ -1,2 +1,25 @@
-package com.kodilla.collections.interfaces;public class Triangle {
+package com.kodilla.collections.interfaces;
+
+public class Triangle implements Shape{
+
+    private double width;
+    private double height;
+    private double hypotenuse;
+
+    public Triangle( double width, double height, double hypotenuse){
+        this.height = height;
+        this.width = width;
+        this.hypotenuse = hypotenuse;
+    }
+
+
+    @Override
+    public double getArea() {
+        return width * height/ 2;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return height+ width + hypotenuse;
+    }
 }
