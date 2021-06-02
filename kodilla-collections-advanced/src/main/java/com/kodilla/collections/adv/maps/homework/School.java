43d71c5a0475418336178ed335b0e3e0cded381e
Marta Karpisiak <mkarpisiak88@gmail.com>
2021-06-02 21:04:14 +0200

    5.2

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/School.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/School.java
@@ -1,2 +1,28 @@
-package com.kodilla.collections.adv.maps.homework;public class School {
+package com.kodilla.collections.adv.maps.homework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class School {
+
+    List<Integer> numberOfStudents = new ArrayList<>();
+
+    public School (Integer... quantityOfStudentsInClasses ) {
+        for (int studentsInClass : quantityOfStudentsInClasses)
+            this.numberOfStudents.add(studentsInClass);
+    }
+
+
+    public int getSum() {
+        int sum = 0;
+        for (int studentsInClass : numberOfStudents )
+            sum += studentsInClass;
+        return sum;
+    }
+
+
+    @Override
+    public String toString() {
+        return "School: " + numberOfStudents.toString();
+    }
 }
--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/SchoolDirectory.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/SchoolDirectory.java
@@ -1,2 +1,32 @@
-package com.kodilla.collections.adv.maps.homework;public class SchoolDirectory {
+package com.kodilla.collections.adv.maps.homework;
+
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SchoolDirectory {
+    public static void main(String[] args) {
+
+        Map<Principal, School> school = new HashMap<>();
+        Principal uam = new Principal("Jan", "Kowalski");
+        Principal awf = new Principal("Adam", "Nowak");
+        Principal ue =  new Principal("Eryk", "Majewicz");
+
+        School uamSum = new School(27, 32, 12, 54, 120);
+        School awfSum = new School(55, 76, 26, 94);
+        School ueSum = new School(44, 57, 36, 87, 98);
+
+        school.put(uam, uamSum);
+        school.put(awf, awfSum);
+        school.put(ue, ueSum);
+
+        for (Map.Entry<Principal, School> principalEntry : school.entrySet())
+            System.out.println(principalEntry.getKey().getFirstname() + " " +
+                    principalEntry.getKey().getLastname()+"- there is " +
+                    principalEntry.getValue().getSum() + " students in his college.");
+
+
+    }
+
 }
