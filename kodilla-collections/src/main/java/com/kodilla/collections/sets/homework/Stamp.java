a3d68bcc7ff8e164f5ddb80efe02d6af68299a3f
Marta Karpisiak <mkarpisiak88@gmail.com>
2021-06-02 20:50:28 +0200

    4.4

--- kodilla-collections/src/main/java/com/kodilla/collections/sets/homework/Stamp.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/sets/homework/Stamp.java
@@ -1,18 +1,36 @@
 package com.kodilla.collections.sets.homework;
 
+import java.util.Objects;
+
 public class Stamp {
     private String name;
     private double height;
     private double width;
     private boolean clean;
 
-public Stamp (String name, double height, double width, boolean clean){
-    this.name= name;
+public Stamp (String name, double height, double width, boolean clean) {
+    this.name = name;
     this.height = height;
     this.width = width;
     this.clean = clean;
-
 }
+@Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Stamp stamp = (Stamp) o;
+        return Objects.equals(name, stamp.name)
+                && Objects.equals(height, stamp.height)
+                && Objects.equals(width, stamp.width)
+                && Objects.equals(clean, stamp.clean);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, height, width, clean);
+    }
 
     public String getName() {
         return name;
