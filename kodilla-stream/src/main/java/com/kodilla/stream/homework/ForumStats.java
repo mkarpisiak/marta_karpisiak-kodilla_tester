42588a59c490d6e4495c53f1745c1bd7037ff5f3
Marta Karpisiak <mkarpisiak88@gmail.com>
2021-06-30 17:18:40 +0200

    6.3

--- kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
@@ -1,2 +1,26 @@
-package com.kodilla.stream.homework;public class ForumStats {
+package com.kodilla.stream.homework;
+
+import com.kodilla.stream.UsersRepository;
+
+public class ForumStats {
+    public static void main(String[] args) {
+        double avg = UsersRepository.getUsersList()
+                .stream()
+                .filter(u->u.getAge()>=40)
+                .mapToInt(u->u.getNumberOfPost())
+                .average()
+                .getAsDouble();
+        System.out.println("Srednia liczba postow dla uzytkownikow, ktorych wiek jest >= 40 to: " + avg);
+
+        double avg2 = UsersRepository.getUsersList()
+                .stream()
+                .filter(u->u.getAge()<40)
+                .mapToInt(u->u.getNumberOfPost())
+                .average()
+                .getAsDouble();
+        System.out.println("Srednia liczba postow dla uzytkownikow, ktorych wiek jest < 40 to: " + avg2);
+
+    }
+
+
 }
